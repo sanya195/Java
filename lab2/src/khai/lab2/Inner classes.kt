d6ee6762f439e7023901faa6c64910717e6d8910
sanya195 <zhuker1@gmail.com>
2017-12-16 02:56:55 +0200

    Add files via upload

---
+++ lab2/src/khai/lab2/Inner classes.kt
@@ -0,0 +1,11 @@
+package khai.lab2
+
+class Project {
+    val stage: Double = 0.1
+}
+
+class Curiosity{
+    fun assess() {
+        println("Student is interested.")
+    }
+}
---
+++ lab2/src/khai/lab2/Interfaces.kt
@@ -0,0 +1,17 @@
+package khai.lab2
+
+interface Diploma {
+
+    val theme: String
+
+    fun defence()
+
+}
+
+interface Exam {
+
+    var mark: Int
+
+    fun pass()
+
+}
